942ace4f0c75c6873f4c403a36445d063dfff286
Kavya Manohar <sakhi.kavya@gmail.com>
2018-08-25 17:37:32 +0530

    Add test for syllablizer

---
+++ test/syllablizer-test.py
@@ -0,0 +1,53 @@
+import csv
+import unittest
+import sys
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, '../python')
+from fsa import Fsa
+
+
+class AnalyserGeneratorTests(unittest.TestCase):
+
+    def setUp(self):
+        self.csvfile = open('syllable-test.tsv')
+        automata = '../syllablizer.a'
+        dialect = csv.Sniffer().sniff(self.csvfile.read(1024))
+        # rewind
+        self.csvfile.seek(0)
+        self.data = csv.reader(self.csvfile, dialect)
+        self.fsa = Fsa(automata)
+
+    def tearDown(self):
+        self.csvfile.close()
+
+    def test_analyse(self):
+        for row in self.data:
+            with self.subTest():
+                anals = self.fsa.generate(row[0])
+                match = False
+                self.assertTrue(len(anals) != 0,
+                                'Analysis failed for ' + row[0])
+                print(row[0], '\t-->\t', anals)
+                for index in range(len(anals)):
+                    if row[1] == anals[index][0]:
+                        match = True
+                        break
+                self.assertEqual(match, True, 'Analysis for ' + row[1])
+
+    def test_generate(self):
+        for row in self.data:
+            with self.subTest():
+                match = False
+                gens = self.fsa.analyse(row[1])
+                self.assertTrue(
+                    len(gens) != 0, 'Generate failed for ' + row[1])
+                print(row[1], '\t<--\t', gens)
+                for index in range(len(gens)):
+                    if row[0] == gens[index][0]:
+                        match = True
+                        break
+                self.assertEqual(match, True, 'Generate for ' + row[1])
+
+if __name__ == '__main__':
+    unittest.main()
