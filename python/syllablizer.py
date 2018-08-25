59981aadb97d7d40aed8732b770cdbd0d78dc87e
Kavya Manohar <sakhi.kavya@gmail.com>
2018-08-25 14:48:37 +0530

    python script for syllable splitter

--- python/syllablizer.py
+++ python/syllablizer.py
@@ -9,9 +9,9 @@ def main():
 	a.add_argument('-i', '--input', metavar="INFILE", type=open,
 					dest="infile", help="source of malayalam text to be analysed")
 	a.add_argument('-o', '--output', metavar="OUTFILE", type=argparse.FileType('w+', encoding='UTF-8'),
-					dest="outfile", help="target of generated IPA")
+					dest="outfile", help="target of splitted syllables")
 	options = a.parse_args()
-	fsa = os.path.dirname(os.path.realpath(__file__))+'/../IPAGenerator.a'
+	fsa = os.path.dirname(os.path.realpath(__file__))+'/../syllablizer.a'
 	IPA_generator = Fsa(fsa)
 	if not options.infile:
 		options.infile = stdin
