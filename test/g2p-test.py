5956736f9927500b4233d89394262a28c8e57168
Kavya Manohar <sakhi.kavya@gmail.com>
2020-10-09 11:38:59 +0530

    Update test for g2p and p2g conversions

--- test/g2p-test.py
+++ test/g2p-test.py
@@ -6,32 +6,29 @@ from mlphon import PhoneticAnalyser
 
 CURR_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
-class AnalyserTests(unittest.TestCase):
 
+class G2PTests(unittest.TestCase):
     def setUp(self):
-        self.csvfile = open(os.path.join(CURR_DIR, 'data_g2p.tsv'))
-        dialect = csv.Sniffer().sniff(self.csvfile.read(1024))
-        # rewind
-        self.csvfile.seek(0)
-        self.data_g2p = csv.reader(self.csvfile, dialect)
+        self.csvfile = open(os.path.join(CURR_DIR, "data_g2p.tsv"))
+        self.data_g2p = csv.reader(self.csvfile, delimiter="\t")
         self.mlphon = PhoneticAnalyser()
 
     def tearDown(self):
         self.csvfile.close()
 
-    def test_analyse(self):
+    def test_g2p(self):
         for row in self.data_g2p:
             with self.subTest():
-                anals = self.mlphon.grapheme_to_phoneme(row[0])
+                result_g2p = self.mlphon.grapheme_to_phoneme(row[0])
                 match = False
-                self.assertTrue(len(anals) != 0,
-                                'Analysis failed for ' + row[0])
-                print(row[0], '\t-->\t', anals)
-                for index in range(len(anals)):
-                    if row[1] == str(anals):
+                self.assertTrue(len(result_g2p) != 0, "Analysis failed for " + row[0])
+                print(row[0], "\t-->\t", result_g2p)
+                for index in range(len(result_g2p)):
+                    if row[1] == str(result_g2p):
                         match = True
                         break
-                self.assertEqual(match, True, 'Analysis for ' + row[0])
+                self.assertEqual(match, True, "Analysis for " + row[0])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
--- test/p2g-test.py
+++ test/p2g-test.py
@@ -6,33 +6,30 @@ from mlphon import PhoneticAnalyser
 
 CURR_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
-class AnalyserTests(unittest.TestCase):
 
+class P2GTests(unittest.TestCase):
     def setUp(self):
-        self.csvfile = open(os.path.join(CURR_DIR, 'data_p2g.tsv'))
-        # Correct the csv read file using tab delimiter
-        dialect = csv.Sniffer().sniff(self.csvfile.read(1024))
-        # rewind
-        self.csvfile.seek(0)
-        self.data_p2g = csv.reader(self.csvfile, dialect)
+        self.csvfile = open(os.path.join(CURR_DIR, "data_p2g.tsv"))
+        self.data_p2g = csv.reader(self.csvfile, delimiter="\t")
         self.mlphon = PhoneticAnalyser()
 
     def tearDown(self):
         self.csvfile.close()
 
-    def test_analyse(self):
+    def test_p2g(self):
+        print(self.data_p2g)
         for row in self.data_p2g:
             with self.subTest():
-                anals = self.mlphon.phoneme_to_grapheme(row[0])
+                result_p2g = self.mlphon.phoneme_to_grapheme(row[0])
                 match = False
-                self.assertTrue(len(anals) != 0,
-                                'Analysis failed for ' + row[0])
-                print(row[0], '\t-->\t', anals)
-                for index in range(len(anals)):
-                    if row[1] == str(anals):
+                self.assertTrue(len(result_p2g) != 0, "Analysis failed for " + row[0])
+                print(row[0], "\t-->\t", result_p2g)
+                for index in range(len(result_p2g)):
+                    if row[1] == str(result_p2g):
                         match = True
                         break
-                self.assertEqual(match, True, 'Analysis for ' + row[0])
+                self.assertEqual(match, True, "Analysis for " + row[0])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
